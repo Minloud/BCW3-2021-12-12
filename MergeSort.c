a725e2e73d1f47b726ee7bd30d81f8e6bba96098
Minloud <40829737+Minloud@users.noreply.github.com>
2021-12-12 13:54:04 +0200

    Merge sort edit ohter

--- MergeSort.c
+++ MergeSort.c
@@ -1,11 +1,17 @@
 
 void mergeSort(int* array, int lo, int hi) {
-	int mid = (lo + hi) / 2;
+	int center = (lo + hi) / 2;
 
-	if ( lo < mid ) {
-		mergeSort(array, lo, mid);
-		mergeSort(array, mid, hi);
+	if ( lo < center ) {
+		mergeSort(array, lo, center);
+		mergeSort(array, center, hi);
 	}
 
-	merge(array, lo, mid, hi);
+	merge(array, lo, center, hi);
+}
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	printf("EPIC WIN\n");
+	return 0;
 }
